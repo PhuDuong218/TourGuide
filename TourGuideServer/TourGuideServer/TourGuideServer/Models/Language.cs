e417cc5e1cbf891ea7e23863491fb86b9614ca99
TuyetNhi <nhiphan171005@gmail.com>
2026-04-05 19:22:12 +0700

    Xóa thư mục TourGuideServer và file zip khỏi repo

--- TourGuideServer/TourGuideServer/TourGuideServer/Models/Language.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TourGuideServer.Models
-{
-    public class Language
-    {
-        public string LanguageCode { get; set; } = string.Empty;
-        public string LanguageName { get; set; } = string.Empty;
-    }
-}
